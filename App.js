6caa03a5237cfbaf0b8de194b0aee2bd070a0ccb
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2020-12-26 05:09:02 +0530

    Add Start Screen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from "./components/Header"
+import StartGameScreen from "./components/StartGameScreen"
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header/>
+      <StartGameScreen/>
     </View>
   );
 }
--- components/StartGameScreen.js
+++ components/StartGameScreen.js
@@ -18,16 +18,55 @@
 //---------------------------------------------------------------------------------------------
 
 import React from "react"
-import {View, Text,StyleSheet} from "react-native"
+import {View, Text,StyleSheet, TextInput,Button} from "react-native"
 
-const StartGameScreen = ()=>{
+const StartGameScreen = (props)=>{
     return (
-        <View style={styles.}></View>
+        <View style={styles.screen}>
+        <Text style={styles.title}>Start A New Game!</Text>
+        <View style={styles.inputContainer}>
+            <Text >Select A Number</Text>
+            <TextInput/>
+            <View style={styles.buttonContainer}>
+                <Button title="Reset"/>
+                <Button title="confirm"/>
+            </View>
+        </View>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    
+    screen:{
+        flex:1,
+        padding:10,
+        alignItems:"center",
+        justifyContent:"flex-start",
+
+    },
+    title:{
+        fontSize:20,
+        marginVertical:15
+    },
+    inputContainer:{
+        width:300,
+        maxWidth:"80%",
+        alignItems:"center",
+        shadowColor:"black",
+        shadowOffset:{width:0,height:2},
+        shadowOpacity:0.5,
+        shadowRadius: 6,
+        elevation: 8,
+        backgroundColor:"white",
+        padding:20,
+        borderRadius:10
+    },
+    buttonContainer:{
+        flexDirection:"row",
+        width:"100%",
+        justifyContent:"space-between",
+        paddingHorizontal:15
+    }
 })
 
 export default StartGameScreen
