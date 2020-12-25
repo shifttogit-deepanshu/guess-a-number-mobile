c316a3605ca02cc6d28b65224392b0359197dd35
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2020-12-26 04:12:14 +0530

    create start screen boilerplate

---
+++ components/StartGameScreen.js
@@ -0,0 +1,35 @@
+//Boilerplate----------------------------------------------------------------------------------
+
+// import React from "react"
+// import {View, Text,StyleSheet} from "react-native"
+
+// const StartGameScreen = ()=>{
+//     return (
+//         <View></View>
+//     )
+// }
+
+// const styles = StyleSheet.create({
+    
+// })
+
+// export default StartGameScreen
+
+//---------------------------------------------------------------------------------------------
+
+import React from "react"
+import {View, Text,StyleSheet} from "react-native"
+
+const StartGameScreen = ()=>{
+    return (
+        <View style={styles.}></View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default StartGameScreen
+
+
